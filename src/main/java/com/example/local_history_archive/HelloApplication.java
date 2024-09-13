3c3e237207652f4c345989d6a26eb5f8f5c75c12
mKikumori <matheuskiku@gmail.com>
2024-09-13 18:52:08 +1000

    commit 1

--- src/main/java/com/example/local_history_archive/HelloApplication.java
+++ src/main/java/com/example/local_history_archive/HelloApplication.java
@@ -1,7 +1,7 @@
 package com.example.local_history_archive;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.fxml.*;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -12,10 +12,11 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
+        stage.setTitle("Home page");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
