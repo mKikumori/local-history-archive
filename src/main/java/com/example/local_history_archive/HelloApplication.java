95f55be1d4716aadf2c2596f3461a22a07698a33
mKikumori <matheuskiku@gmail.com>
2024-09-13 17:16:42 +1000

    test commit

--- src/main/java/com/example/local_history_archive/HelloApplication.java
+++ src/main/java/com/example/local_history_archive/HelloApplication.java
@@ -7,6 +7,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+// Test
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
