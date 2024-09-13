8f0fbc95570f829f3eb4be9fa864379a40fb4c67
Yahzee Gerardo <haevlure@gmail.com>
2024-09-14 02:45:33 +1000

    Upload and Homepage Draft

--- src/main/java/com/example/local_history_archive/HelloApplication.java
+++ src/main/java/com/example/local_history_archive/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.*;
 
 import java.io.IOException;
 
@@ -13,6 +14,8 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        String css = this.getClass().getResource("application.css").toExternalform();
+        scene.getStylesheets().add(css);
         stage.setTitle("Local History Archive");
         stage.setScene(scene);
         stage.show();
