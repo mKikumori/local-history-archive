7dd6c667b87ab435625cc488dfac3038bc279d65
Yahzee Gerardo <haevlure@gmail.com>
2024-09-14 01:07:06 +1000

    Login Button

--- src/main/java/com/example/local_history_archive/HelloApplication.java
+++ src/main/java/com/example/local_history_archive/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-        stage.setTitle("Unlogged in Homepage");
+        stage.setTitle("Local History Archive");
         stage.setScene(scene);
         stage.show();
     }
