17152b4f5f8a2238d6fb46aad13151c072fb1ea0
mKikumori <matheuskiku@gmail.com>
2024-09-14 18:39:48 +1000

    Login and Register

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,13 @@
 module com.example.local_history_archive {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.local_history_archive to javafx.fxml;
     exports com.example.local_history_archive;
+    exports com.example.local_history_archive.controller;
+    opens com.example.local_history_archive.controller to javafx.fxml;
+    exports com.example.local_history_archive.model;
+    opens com.example.local_history_archive.model to javafx.fxml;
 }
