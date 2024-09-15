bd3bb20a16022ff9be601946f5a175ac46d7454e
Yahzee Gerardo <haevlure@gmail.com>
2024-09-15 18:51:35 +1000

    css trial

--- src/main/java/com/example/local_history_archive/HelloApplication.java
+++ src/main/java/com/example/local_history_archive/HelloApplication.java
@@ -18,6 +18,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
+        //scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
         stage.setTitle(TITLE);
         stage.setScene(scene);
         stage.show();
