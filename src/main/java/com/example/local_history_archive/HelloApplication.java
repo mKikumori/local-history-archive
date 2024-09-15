28f0ced3a681376b8b78071d1cc53c7d55f9dd51
mKikumori <matheuskiku@gmail.com>
2024-09-15 14:08:19 +1000

    Login and Register

--- src/main/java/com/example/local_history_archive/HelloApplication.java
+++ src/main/java/com/example/local_history_archive/HelloApplication.java
@@ -4,25 +4,25 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.*;
 
 import java.io.IOException;
 
+// Shouldn't change
 public class HelloApplication extends Application {
+
     public static final String TITLE = "Local History Archive";
-    public static final int WIDTH = 1280;
+    public static final int WIDTH = 1200;
     public static final int HEIGHT = 800;
-    
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("searchpage-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml")); // Home Page
         Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
         stage.setTitle(TITLE);
         stage.setScene(scene);
         stage.show();
     }
-    
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 }
